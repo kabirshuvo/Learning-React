f271277039c7e19f656bc24e6e627bd8d5f427ba
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-20 23:52:49 +0600

    My first Component is Working

--- reactjs-core-concepts/src/App.js
+++ reactjs-core-concepts/src/App.js
@@ -1,30 +1,26 @@
 import './App.css';
-import logo from './logo.svg';
 
-const h1Style = {
-  color: 'rebeccapurple'
-}
+// const h1Style = {
+//   color: 'rebeccapurple'
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{color: 'rebeccapurple'}}>
-          Learning React
-        </p>
-        <h1 style={h1Style}>Ki Ase Jibone ?</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <h1> Learning React</h1>
+      <Motizhil></Motizhil>
     </div>
-  );
+  )
+    
 }
 
+function Motizhil (){
+  return(
+    <div>
+      <h1>Location: Motizhil</h1>
+      <p>Red Crisent House: level - 10</p>
+    </div>
+  )
+}
+App();
 export default App;
