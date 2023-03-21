f380eb85a1456e62f69a6111897b3cb8310be9e2
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-21 20:10:24 +0600

    First time I fetch url in react

--- reactjs-core-concepts2/src/App.js
+++ reactjs-core-concepts2/src/App.js
@@ -1,26 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
-function Counter(){
-  const [count, setCount] = useState(0);
-  const increaseCount = () => setCount(count + 1);
-  const decreaseCount = () => setCount(count - 1);
+
+function ExternalUsers (){
+  const [users, setUsers] = useState([]);
+  useEffect( ()=>{
+    const URL = 'https://jsonplaceholder.typicode.com/users'
+    fetch(URL).then(resp => resp.json()).then(data => setUsers(data))
+  }, [])
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={increaseCount}>Increase</button>
-      <button onClick={decreaseCount}>Decrease</button>
+      <h2>External Users: </h2>
+      {/* <p>{users.length}</p> */}
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
     </div>
   )
 }
+function User (props){
+  return (
+    <div style={{border: '2px solid silver', margin: '1rem', padding: "1rem", backgroundColor: 'rebeccapurple', borderRadius: '0.4rem', color: 'whitesmoke'}}>
+      <h3>Name:{props.name}</h3>
+      <p>Email:{props.email}</p>
+    </div>
+  )
+}
+// function Counter(){
+//   const [count, setCount] = useState(0);
+//   const increaseCount = () => setCount(count + 1);
+//   const decreaseCount = () => setCount(count - 1);
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={increaseCount}>Increase</button>
+//       <button onClick={decreaseCount}>Decrease</button>
+//     </div>
+//   )
+// }
 
 // const products = [
 //   {name: "Laptop", Price: "45000", color: "Silver"},
