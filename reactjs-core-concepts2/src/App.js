7fb59f12a428fadd57f7879a40a5dd3ecec91daa
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-21 15:48:27 +0600

    My counter is working

--- reactjs-core-concepts2/src/App.js
+++ reactjs-core-concepts2/src/App.js
@@ -1,33 +1,53 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-const products = [
-  {name: "Laptop", Price: "45000", color: "Silver"},
-  {name: "Desktop", Price: "165000", color: "Black"},
-  {name: "Watch", Price: "5500", color: "Silver"},
-  {name: "Mobile", Price: "32000", color: "Blue"},
-  {name: "Sunglass", Price: "3500", color: "Black"},
-  {name: "Shoe", Price: "18000", color: "Khaki"}
-];
+
   return (
     <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.Price} color={product.color}></Product>)
-      }
-      {/* <Product name="Laptop" price="45000"></Product>
-      <Product name="Desktop" price="165000"></Product>
-      <Product name="Watch" price="5500"></Product> */}
+      <Counter></Counter>
     </div>
   );
 }
-
-function Product(props){
+function Counter(){
+  const [count, setCount] = useState(0);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
   return (
-    <div className='product'>
-      <h3>Name: {props.name} </h3>
-      <div className='priceNcolorDiv'><p>Price: {props.price} </p> <p>Color: {props.color}</p></div>
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   )
 }
+
+// const products = [
+//   {name: "Laptop", Price: "45000", color: "Silver"},
+//   {name: "Desktop", Price: "165000", color: "Black"},
+//   {name: "Watch", Price: "5500", color: "Silver"},
+//   {name: "Mobile", Price: "32000", color: "Blue"},
+//   {name: "Sunglass", Price: "3500", color: "Black"},
+//   {name: "Shoe", Price: "18000", color: "Khaki"}
+// ];
+
+// /*
+// {
+//         products.map(product => <Product name={product.name} price={product.Price} color={product.color}></Product>)
+//       }
+//       {/* <Product name="Laptop" price="45000"></Product>
+//       <Product name="Desktop" price="165000"></Product>
+//       <Product name="Watch" price="5500"></Product> */}
+
+// */
+
+// function Product(props){
+//   return (
+//     <div className='product'>
+//       <h3> {props.name} </h3>
+//       <div className='priceNcolorDiv'><p>Price: {props.price} </p> <p>Color: {props.color}</p></div>
+//     </div>
+//   )
+// }
 
 export default App;
